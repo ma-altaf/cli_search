30bb4f329dbcb7da424f6f513fa640b86ffe85be
ma-altaf <79712014+ma-altaf@users.noreply.github.com>
2025-04-04 15:52:25 -0700

    Implemented querying

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ fn main() {
 
     let lines = [
         "line 1",
+        "line not",
         "line 2",
         "not a line",
         // "lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
@@ -22,12 +23,13 @@ fn main() {
 
     println!("{:?}", trie.list());
 
-    let engine = trie.engine();
+    let mut engine = trie.engine();
     let mut input = String::new();
 
     loop {
         stdin().read_line(&mut input).unwrap();
-
         engine.query(input.trim().chars().last().unwrap());
+
+        println!("history:\n {:?}", engine.options());
     }
 }
--- src/search_trie.rs
+++ src/search_trie.rs
@@ -78,22 +78,98 @@ impl SearchTrie {
   }
 }
 
+
+#[derive(Debug)]
 pub struct PathNode {
   val: char,
   in_query: bool
 }
 
+impl Clone for PathNode {
+    fn clone(&self) -> Self {
+        Self { val: self.val.clone(), in_query: self.in_query.clone() }
+    }
+}
+
+fn traverse<'a>(node: &'a TrieNode, target: char, path: &mut Vec<PathNode>) -> Vec<(&'a TrieNode, Vec<PathNode>)> {
+  let mut options: Vec<(&'a TrieNode, Vec<PathNode>)> = Vec::new();
+
+  for next in &node.val {
+    if next.0 == &target {
+      path.push(PathNode { val: *next.0, in_query: true });
+      options.push((node.val.get(&target).unwrap(), path.to_vec()));
+    } else {
+      path.push(PathNode { val: *next.0, in_query: false });
+      options.append(&mut traverse(next.1, target, path));
+    }
+    path.pop();
+  }
+
+  return options
+}
+
+fn build_path(path: &Vec<PathNode>) -> String {
+  path.iter().fold(String::new(), |mut acc, PathNode { val, in_query }| {
+    let next_char = match in_query {
+      true => &format!("-{}-", val),
+      false => &format!("{}", val),
+    };
+    acc.push_str(next_char);
+    acc
+  })
+}
+
+fn expand(node: &TrieNode) -> Vec<String> {
+  let mut res = Vec::new();
+
+  for (c, node) in &node.val {
+    let path = c.to_string();
+    let fut = expand(node);
+    
+    if fut.len() > 0 {
+      fut.iter().for_each(|v| {
+        res.push(format!("{}{}", path, v));
+      });
+    } else {
+      res.push(path);
+    }
+  }
+
+  res
+}
+
 pub struct Engine<'a> {
-  // store the different current Trienodes reached and PathNodes to build the line
-  history: Vec<(&'a TrieNode, Vec<PathNode>)>
+  // store the different current Trie_nodes reached and PathNodes to build the line with history for backspace
+  history: Vec<Vec<(&'a TrieNode, Vec<PathNode>)>>
 }
 
 impl<'a> Engine<'a> {
   pub fn new(root: &'a TrieNode) -> Self {
-    Self { history:  vec![(root, Vec::new())]}
+    Self { history:  vec![vec![(root, Vec::new())]]}
   }
 
-  pub fn query(&self, v: char) {
-    
+  pub fn query(&mut self, input: char) {
+    let curr = self.history.pop().unwrap();
+    let mut next: Vec<(&'a TrieNode, Vec<PathNode>)> = Vec::new();
+
+    for (node, mut path) in curr {
+      next.append(&mut traverse(node, input, &mut path));
+    }
+
+    self.history.push(next);
+  }
+
+  pub fn options(&self) -> Vec<String> {
+    let mut options_list = Vec::new();
+    if let Some(nodes) = self.history.last() {
+      for (node, path) in nodes {
+        let path1 = build_path(path);
+        expand(node).iter().for_each(|path2| {
+          options_list.push(format!("{}{}", path1, path2));
+        });
+      }
+    }
+
+    options_list
   }
 }
